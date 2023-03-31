1e9d0f7e76aa78fe782aff5ec6ad0b5178b660bf
Umesh Sikka <umeshsikka0123@gmail.com>
2023-03-31 12:56:17 +0530

    resetP

--- controllers/authController.js
+++ controllers/authController.js
@@ -107,9 +107,15 @@ exports.restrictTo = (...roles) => {
   };
 };
 
-exports.forgotPassword = (req, res, next) => {
+exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with this email address', 404));
+  }
   // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
   // 3) Send it to user's email
-};
-exports.forgotPassword = (req, res, next) => {};
+});
+exports.resetPassword = (req, res, next) => {};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.post('/forgetPassword', authController.login);
-router.post('/resetPassword', authController.login);
+router.post('/forgetPassword', authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
 
 router
   .route('/')
